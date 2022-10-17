586ec0d342b622ddee1fdb1d209a7fc9ba3fe56e
Neimaad <damienlaoussing@hotmail.com>
2022-10-17 15:12:05 +0200

    first commit

---
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_api/ProjetIjva500SoaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIjva500SoaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIjva500SoaApiApplication.class, args);
+	}
+
+}
