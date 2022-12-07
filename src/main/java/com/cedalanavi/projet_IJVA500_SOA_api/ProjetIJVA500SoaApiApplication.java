0f2796425aa1a05783f24c215c11d580e5e2ae97
Neimaad <damienlaoussing@hotmail.com>
2022-12-08 00:41:07 +0100

    refacto code

--- src/main/java/com/cedalanavi/projet_IJVA500_SOA_api/ProjetIJVA500SoaApiApplication.java
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_api/ProjetIJVA500SoaApiApplication.java
@@ -1,27 +1,12 @@
 package com.cedalanavi.projet_IJVA500_SOA_api;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProjetIJVA500SoaApiApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetIJVA500SoaApiApplication.class, args);
-	}
-	
-	@Bean(name = "myRestTemplate")
-	public RestTemplate collectCentRestTemplate(RestTemplateBuilder builder, HttpServletRequest httpServletRequest) {
-	    return builder.rootUri("/**")
-	            .additionalInterceptors((ClientHttpRequestInterceptor) (request, body, execution) -> {
-	                request.getHeaders().add("Authorization", httpServletRequest.getHeader("Authorization"));
-	                return execution.execute(request, body);
-	            }).build();
 	}
 }
