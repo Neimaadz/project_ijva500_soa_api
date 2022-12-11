b388e2a9256c747eff258d4e168ababb6557e6fd
Neimaad <damienlaoussing@hotmail.com>
2022-12-11 22:44:57 +0100

    add and import project utils dependency

--- src/main/java/com/cedalanavi/projet_IJVA500_SOA_api/ProjetIJVA500SoaApiApplication.java
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_api/ProjetIJVA500SoaApiApplication.java
@@ -2,8 +2,12 @@ package com.cedalanavi.projet_IJVA500_SOA_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.cedalanavi.projet_IJVA500_SOA_utils.RestTemplateConfig;
 
 @SpringBootApplication
+@Import(RestTemplateConfig.class)
 public class ProjetIJVA500SoaApiApplication {
 	
 	public static void main(String[] args) {
